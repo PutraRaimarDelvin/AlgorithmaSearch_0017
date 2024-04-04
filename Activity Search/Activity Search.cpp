ae617ffcebc0a2d8cc1967e072cb048f1c2feeb5
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-04-04 19:15:28 +0700

    18

--- Activity Search/Activity Search.cpp
+++ Activity Search/Activity Search.cpp
@@ -5,43 +5,22 @@ int arr[20]; //array to be searched
 int n;		 //number of elements in the array
 int i;		 //index of array element
 
-
-void LinearSearch()
+void display()
 {
+	cout << " array element are: ";
+	for (1 = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 
-	char ch;
-	int ctr; //number of comparisons
-	int item;
-
-	do
-	{
-		//accept the number to be searched
-		cout << "\nEnter the element you want to search: ";//step 1
-		cin >> item;
-
-		ctr = 0;
-		i = 0;// step 2
-		while (i < n) // step 3
-		{
-			ctr++;
-			if (arr[i] == item)
-			{
-				cout << "\n" << item " found at position " << (i + 1) << endl; //step 6
-				break;
-			}
-			i++;//Step 4
-		}
-		if (i == n) //step 5
-			cout << "\n" << item << " not found in the array\n";
-		cout << "\nNumber of comparisons: " << ctr << endl;
-
-		cout << "\nContinue search(y/n): ";
-		cin >> ch;
-
-	} while ((ch == 'y') || (ch == 'Y'));
 }
 
 
+int main()
+{
+	input();
+	LinearSearch();
+	display();
+}
 
 
 
