ea5f85f761b34f51210a6f34984519dd5ce19ae8
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-04-04 18:45:55 +0700

    3

--- Activity Search/Activity Search.cpp
+++ Activity Search/Activity Search.cpp
@@ -4,3 +4,18 @@ using namespace std;
 int arr[20]; //array to be searched
 int n;		 //number of elements in the array
 int i;		 //index of array element
+
+void input()
+{
+	while (true)
+	{
+		cout << "enter the number of elements in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\narray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+
+	
+}
