31031aa0232729773cb3c9d22c27e3befbfe687a
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-04-04 19:11:38 +0700

    14

--- Activity Search/Activity Search.cpp
+++ Activity Search/Activity Search.cpp
@@ -5,11 +5,10 @@ int arr[20]; //array to be searched
 int n;		 //number of elements in the array
 int i;		 //index of array element
 
-void display()
-{
-	cout << " array element are: ";
-	for (1 = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
 
+int main()
+{
+	input();
+	LinearSearch();
+	display();
 }
