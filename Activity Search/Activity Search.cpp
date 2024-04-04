0af93a0a0648d41a7c8c500441879a8000f46e03
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-04-04 18:48:09 +0700

    5

--- Activity Search/Activity Search.cpp
+++ Activity Search/Activity Search.cpp
@@ -5,26 +5,11 @@ int arr[20]; //array to be searched
 int n;		 //number of elements in the array
 int i;		 //index of array element
 
-void input()
-{
-	while (true)
-	{
-		cout << "enter the number of elements in the array: ";
-		cin >> n;
-		if ((n > 0) && (n <= 20))
-			break;
-		else
-			cout << "\narray should have minimum 1 and maximum 20 elements.\n\n";
-	}
 
-	//accept array elements
-	cout << "\n-----------------\n";
-	cout << "enter array element\n";
-	cout << "\n-----------------\n";
-	for (i = 0; i < n; i++)
-	{
-		cout << "<" << (i + 1) << ">";
-		cin >> arr[i];
-	}
-	
+void LinearSearch()
+{
+
+	char ch;
+	int ctr; //number of comparisons
+	int item;
 }
